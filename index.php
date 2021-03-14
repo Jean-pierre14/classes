0a52bbb629854829993998d5732a8bcd856b4b04
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-03-15 00:37:12 +0200

    Add new class form html and css

--- index.php
+++ index.php
@@ -35,14 +35,30 @@
             <section>
                 <!-- <h3>section side</h3> -->
                 <?php if(isset($_GET['add'])):?>
-                    <h3>Add form</h3>
-                    <?php if($_GET['add'] == 'addClass'):?>
-                        <h3>add class</h3>
-                    <?php elseif($_GET['add'] == 'addstudent'):?>
-                        <h4>add student</h4>
-                    <?php endif;?>    
+                <h3>Add form</h3>
+                <?php if($_GET['add'] == 'addClass'):?>
+                <h3>add class</h3>
+                <form action="" class="form-row">
+                    <div class="form-group col-md-12">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Name of the class" value="<?= $name;?>"
+                            class="form-control">
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="class">Class</label>
+                        <select name="class" id="class" name="class">
+                            <option value="">-- select --</option>
+                            <option value="P1">P1</option>
+                            <option value="P2">P2</option>
+                            <option value="P3">P3</option>
+                        </select>
+                    </div>
+                </form>
+                <?php elseif($_GET['add'] == 'addstudent'):?>
+                <h4>add student</h4>
+                <?php endif;?>
                 <?php else:?>
-                        <p class="alert alert-warning">data</p>
+                <p class="alert alert-warning">data</p>
                 <?php endif;?>
             </section>
         </div>
